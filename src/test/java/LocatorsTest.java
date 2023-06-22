5367b6e0d0759cb8d19eed447d3adac2affd72d5
Anna12388 <125131848+Anna12388@users.noreply.github.com>
2023-06-22 22:34:54 +0300

    Initial commit

---
+++ src/test/java/LocatorsTest.java
@@ -0,0 +1,48 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class LocatorsTest extends BaseTest{
+        @Test
+    public void checkLocators(){
+
+            driver.get(URL);
+
+            WebElement usernameField = driver.findElement(By.name("user-name"));
+            WebElement passWord = driver.findElement(By.id("password"));
+            WebElement loginButton = driver.findElement(By.className("btn_action"));
+
+            usernameField.sendKeys("standard_user");
+            passWord.sendKeys("secret_sauce");
+            loginButton.click();
+
+            WebElement productCard = driver.findElement(By.xpath("//*[@id=\"item_4_title_link\"]/div"));
+            WebElement addToCardButton = driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
+            addToCardButton.click();
+
+            driver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a")).click();
+            WebElement checkoutButton = driver.findElement(By.id("checkout"));
+            checkoutButton.click();
+
+            WebElement firstName = driver.findElement(By.id("first-name"));
+            WebElement lastName = driver.findElement(By.id("last-name"));
+            WebElement zipCode = driver.findElement(By.id("postal-code"));
+
+            firstName.sendKeys("Name");
+            lastName.sendKeys("Lastname");
+            zipCode.sendKeys("12345");
+
+            WebElement continueButton = driver.findElement(By.id("continue"));
+            continueButton.click();
+
+            WebElement finishButton = driver.findElement(By.id("finish"));
+            finishButton.click();
+
+            WebElement footer = driver.findElement(By.tagName("footer"));
+            WebElement twitter = driver.findElement(By.linkText("Twitter"));
+            WebElement facebook = driver.findElement(By.partialLinkText("Face"));
+
+            WebElement burgerMenuButton = driver.findElement(By.cssSelector(".bm-burger-button"));
+
+        }
+    }
